eff0c79d604fc5c4faebc460376787905bcb1f54
sujitha-madda <160210518+sujitha-madda@users.noreply.github.com>
2026-05-19 09:09:10 +0530

    Create config.py

---
+++ src/utils/config.py
@@ -0,0 +1,14 @@
+CONFIG = {
+
+    "phi4_model":
+        "microsoft/phi-4",
+
+    "qwen_model":
+        "Qwen/Qwen1.5-1.8B",
+
+    "translation_model":
+        "facebook/nllb-200-distilled-600M",
+
+    "similarity_model":
+        "paraphrase-multilingual-MiniLM-L12-v2"
+}
