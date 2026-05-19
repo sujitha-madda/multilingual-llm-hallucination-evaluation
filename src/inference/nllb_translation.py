a41ea022c5df0cbf55eae4499759213cae6a6560
sujitha-madda <160210518+sujitha-madda@users.noreply.github.com>
2026-05-19 09:03:29 +0530

    Update nllb_translation.py

--- src/inference/nllb_translation.py
+++ src/inference/nllb_translation.py
@@ -1,14 +1,67 @@
-def en_to_target(text):
-    inputs = trans_tokenizer(
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+import torch
+
+TRANS_MODEL = "facebook/nllb-200-distilled-600M"
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+tokenizer = AutoTokenizer.from_pretrained(TRANS_MODEL)
+
+model = AutoModelForSeq2SeqLM.from_pretrained(
+    TRANS_MODEL
+).to(DEVICE)
+
+
+def translate_text(text, tgt_lang):
+    """
+    English -> target language
+    """
+
+    inputs = tokenizer(
         text,
         return_tensors="pt",
-        truncation=True
-    ).to("cuda")
+        truncation=True,
+        max_length=256
+    ).to(DEVICE)
 
-    outputs = trans_model.generate(
+    outputs = model.generate(
         **inputs,
-        forced_bos_token_id=trans_tokenizer.convert_tokens_to_ids(CONFIG["lang_code"]),
+        forced_bos_token_id=tokenizer.convert_tokens_to_ids(tgt_lang),
         max_length=256
     )
 
-    return trans_tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return tokenizer.decode(
+        outputs[0],
+        skip_special_tokens=True
+    )
+
+
+def back_translate_nllb(
+    text,
+    tgt_lang="eng_Latn"
+):
+    """
+    Target language -> English
+    """
+
+    try:
+        inputs = tokenizer(
+            text,
+            return_tensors="pt",
+            truncation=True,
+            max_length=256
+        ).to(DEVICE)
+
+        translated_tokens = model.generate(
+            **inputs,
+            forced_bos_token_id=tokenizer.convert_tokens_to_ids(tgt_lang),
+            max_length=256
+        )
+
+        return tokenizer.decode(
+            translated_tokens[0],
+            skip_special_tokens=True
+        )
+
+    except:
+        return ""
